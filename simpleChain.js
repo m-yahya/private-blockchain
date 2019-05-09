32c1fafc48fa85f98c63e6d2a2f60cb221465dc7
m-yahya <yahya_gis@hotmail.com>
2019-05-09 19:10:22 +0200

    Create Blockchain Class

--- simpleChain.js
+++ simpleChain.js
@@ -1,6 +1,6 @@
 /**
  * block class
- * class with a constructor for block data model
+ * class with a constructor for block
  */
 class Block {
   constructor(data) {
@@ -9,5 +9,19 @@ class Block {
       this.body = data,
       this.time = 0,
       this.previousblockhash = ''
+  }
+}
+
+/**
+ * blockchain class
+ */
+class Blockchain {
+  constructor() {
+    // new chain array
+    this.chain = [];
+  }
+  // add block method
+  addBlock(newBlock) {
+    this.chain.push(newBlock);
   }
 }
